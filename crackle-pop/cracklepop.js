6b3b7cfa5ea6bc2f0a0395e9cd2b05aa43bc4b46
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-03-22 18:49:29 +0100

    cracklepop implementation for Recurse Center application

---
+++ crackle-pop/cracklepop.js
@@ -0,0 +1,15 @@
+function cracklePop(max) {
+    for (let i = 0; i <= max; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("CracklePop");
+        } else if (i % 3 === 0) {
+            console.log("Crackle");
+        } else if (i % 5 === 0) {
+            console.log("Pop");
+        } else {
+            console.log(i);
+        }
+    }
+}
+
+cracklePop(100);
