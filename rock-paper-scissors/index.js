6f2bc901f089baa1284422d4a3239a545f7a609f
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-03-27 19:00:01 +0200

    added hand icons

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -4,6 +4,8 @@ const instructionsEl = document.querySelector(".instructions");
 const rockBtn = document.querySelector("#rock-btn");
 const paperBtn = document.querySelector("#paper-btn");
 const scissorsBtn = document.querySelector("#scissors-btn");
+let computerSymbolEl = document.querySelector(".computer-symbol");
+let playerSymbolEl = document.querySelector(".player-symbol");
 let outcomeEl = document.querySelector("#outcome-el");
 let playerScoreEl = document.querySelector(".player-score-el");
 let computerScoreEl = document.querySelector(".computer-score-el");
@@ -24,7 +26,7 @@ function startGame() {
 function gameCalculations() {
     let compChoice = computerChoice();
     if (compChoice === playerChoice) {
-        outcomeEl.textContent = "It's a tie!";
+        outcomeEl.textContent = `Computer chose ${compChoice}, too! It's a tie!`;
         playerScore += 0.5;
         computerScore += 0.5;
         renderScore();
@@ -45,19 +47,30 @@ function gameCalculations() {
 
 function computerChoice() {
     let choiceNum = Math.floor(Math.random() * weaponChoices.length);
+    if (weaponChoices[choiceNum] === "scissors") {
+        computerSymbolEl.innerHTML = '<i class="fa-solid fa-hand-scissors"></i>';
+    } else if (weaponChoices[choiceNum] === "paper") {
+        computerSymbolEl.innerHTML = '<i class="fa-solid fa-hand"></i>';
+    } else if (weaponChoices[choiceNum] === "rock") {
+        computerSymbolEl.innerHTML = `<span class="fa-flip-horizontal" style="display: inline-block;">
+                                      <i class="fa-solid fa-hand-back-fist fa-rotate-90"></i>`
+    }
     return weaponChoices[choiceNum];
 }
 
 function chooseRock() {
     playerChoice = "rock";
+    playerSymbolEl.innerHTML = '<i class="fa-solid fa-hand-back-fist fa-rotate-90"></i>'
     gameCalculations();
 }
 function choosePaper() {
     playerChoice = "paper";
+    playerSymbolEl.innerHTML = '<i class="fa-solid fa-hand fa-flip-horizontal"></i>';
     gameCalculations();
 }
 function chooseScissors() {
     playerChoice = "scissors";
+    playerSymbolEl.innerHTML = '<i class="fa-solid fa-hand-scissors fa-flip-horizontal"></i>'
     gameCalculations();
 }
 
