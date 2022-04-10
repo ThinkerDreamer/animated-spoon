8fed7c4019893e3091bb26399b6d2b971a47bd6b
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-04-10 19:51:54 +0200

    reorganized js and added comments

--- tic-tac-toe/index.js
+++ tic-tac-toe/index.js
@@ -11,6 +11,26 @@ const player1Input = document.getElementById("player1-input");
 const player2Input = document.getElementById("player2-input");
 const nameSubmit = document.getElementById("name-submit");
 
+let player1Name = "Player 1";
+let player2Name = "Player 2";
+let player1Score = 0;
+let player2Score = 0;
+let player1sTurn = true;
+let player1Symbol = "X";
+let player2Symbol = "O";
+let gameOver = false;
+let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+let winCombos = [   [0, 1, 2],  //horizontal
+                    [3, 4, 5],  //horizontal
+                    [6, 7, 8],  //horizontal
+                    [0, 3, 6],  //vertical
+                    [1, 4, 7],  //vertical
+                    [2, 5, 8],  //vertical
+                    [0, 4, 8],  //diagonal
+                    [2, 4, 6]   //diagonal
+                ];
+
+// Add an event listener to each square according to its ID number
 for (let square of squares) {
     square.addEventListener("click", function() {
         if (!gameOver) {
@@ -24,9 +44,6 @@ for (let square of squares) {
     });
 }
 
-let player1Name = "Player 1";
-let player2Name = "Player 2";
-
 newGameBtn.addEventListener("click", function() {
     overlay.style.display = "flex";
 });
@@ -36,41 +53,26 @@ resetBtn.addEventListener("click", function() {
 });
 
 nameSubmit.addEventListener("click", function(event) {
-    event.preventDefault(); // To prevent the page from refreshing
+    // Prevent the page from refreshing
+    event.preventDefault();
+    // Get players' preferred names, otherwise use "Player 1/2" as defaults
     player1Name = player1Input.value ? player1Input.value : player1Name;
     player2Name = player2Input.value ? player2Input.value : player2Name;
     overlay.style.display = "none";
     startGame();
 })
 
-let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-let player1Score = 0;
-let player2Score = 0;
-let player1sTurn = true;
-let player1Symbol = "X";
-let player2Symbol = "O";
-let gameOver = false;
-let winCombos = [   [0, 1, 2],  //horizontal
-                    [3, 4, 5],  //horizontal
-                    [6, 7, 8],  //horizontal
-                    [0, 3, 6],  //vertical
-                    [1, 4, 7],  //vertical
-                    [2, 5, 8],  //vertical
-                    [0, 4, 8],  //diagonal
-                    [2, 4, 6]   //diagonal
-                ];
-
 function renderBoard() {
     let content = "";
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < squares.length; j++) {
+            // if the symbol is in the array, render it on the DOM
             if (board[i] == player1Symbol) {
                 content = player1Symbol;
             } else if (board[i] == player2Symbol) {
                 content = player2Symbol;
             } else {
-                content = "";
+                content = ""; // otherwise, leave it blank
             }
         }
         squares[i].textContent = content;
@@ -98,22 +100,27 @@ function startGame() {
     player2NameEl.textContent = player2Name;
     messageEl.textContent = `${player1Name}, select a square.`;
     renderBoard();
+    // Hide the newGame button
     newGameBtn.style.display = "none";
 }
 
 function player1Move(squareNum) {
+    // squareNum is number of clicked square
     let move = squareNum;
-    console.log("player1Move: " + board[move]);
+    // If an X or O is in that spot already...
     if (board[move] == player1Symbol || board[move] == player2Symbol) {
         messageEl.textContent = "Invalid move. Please choose an unoccupied square.";
+        // ...make the same player go again
         player1sTurn = true;
     } else {
+        // If it's a valid move, record it
         board[move] = player1Symbol;
         if (checkWin(player1Symbol)) {
             player1Wins();
         } else if (checkTie()) {
             tieGame();
         } else {
+            // If no win, no tie, let the next player go
             player1sTurn = false;
             messageEl.textContent = `${player2Name}, select a square.`;
             renderBoard();
@@ -121,17 +128,22 @@ function player1Move(squareNum) {
     }
 }
 function player2Move(squareNum) {
+    // squareNum is number of clicked square
     let move = squareNum;
+    // If an X or O is in that spot already...
     if (board[move] == player1Symbol || board[move] == player2Symbol) {
         messageEl.textContent = "Invalid move. Please choose an unoccupied square.";
+        // ...make the same player go again
         player1sTurn = false;
     } else {
+        // If it's a valid move, record it
         board[move] = player2Symbol;  
         if (checkWin(player2Symbol)) {
             player2Wins();
         } else if (checkTie()) {
             tieGame();
         } else {
+            // If no win, no tie, let the next player go
             player1sTurn = true;
             messageEl.textContent = `${player1Name}, select a square.`;
             renderBoard();
@@ -140,7 +152,7 @@ function player2Move(squareNum) {
 }
 
 function checkWin(player) {
-    console.log("entering checkWin");
+    // Check each winning combo to see if player has it
     for (let i = 0; i < winCombos.length; i++) {
         if (board[winCombos[i][0]] == player && board[winCombos[i][1]] == player && board[winCombos[i][2]] == player) {
             return true;
@@ -149,14 +161,14 @@ function checkWin(player) {
     return false;
 }
 function checkTie() {
-    console.log("entering checkTie");
+    // Count number of squares with an X or O = fullSquares
     let fullSquares = 0;
     for (let i = 0; i < board.length; i++) {
-        console.log(`board[${i}] = ${board[i]}`);
         if (board[i] == player1Symbol || board[i] == player2Symbol) {
             fullSquares++;
         }
         if (fullSquares == 9) {
+            // If all 9 squares are full with no winner, it's a tie
             return true;
         }
     }
@@ -181,18 +193,21 @@ function tieGame() {
 
 function endGame() {
     gameOver = true;
-    player1sTurn = player1sTurn;
+    player1sTurn = !player1sTurn; // The next player gets a turn to go first -- currently not working
+    // Show reset button
     resetBtn.style.display = "inline-block";
     disableBoard();
 }
 
 function resetGame() {
+    // Remove all X's and O's
     board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     gameOver = false;
     enableBoard();
     messageEl.textContent = `${player1Name}, select a square.`;
     renderBoard();
+    // Hide reset button
     resetBtn.style.display = "none";
 }
 
-disableBoard();
+disableBoard(); // When the page loads, the board is disabled
