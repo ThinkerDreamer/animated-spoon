10f2cd6cda59f0ac9b9a4ecb7a0b87066b874539
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-04-13 12:17:46 +0200

    reorder code, fix bug- baseNum space disappears

--- unit-converter-solo-project/index.js
+++ unit-converter-solo-project/index.js
@@ -1,39 +1,41 @@
 let input = 20;
-
 let baseNum = document.querySelector("#base-num");
-baseNum.textContent = input;
-
-function numInputKeyPress() {
-    let numInput = document.querySelector("#num-input");
-    input = numInput.value;
-    calculateConversion(input);
-}
-
+let numInput = document.querySelector("#num-input");
 let lenResults = document.querySelector("#len-results");
 let volResults = document.querySelector("#vol-results");
 let massResults = document.querySelector("#mass-results");
 
+baseNum.textContent = input;
+
+function numInputKeyPress() {
+    if (numInput.value != "" && numInput.value != " ") {
+        input = numInput.value;
+        baseNum.textContent = input;
+        calculateConversion(input);
+    } else {
+        baseNum.innerHTML = "&nbsp;";
+    }
+}
+
 function calculateConversion(input) {
     let metersResults = input * 3.28084;
     let feetResults = input * 0.3048;
-    metersResults = metersResults.toFixed(3);
-    feetResults = feetResults.toFixed(3);
     let litersResults = input * 0.264172;
     let gallonsResults = input * 3.78541;
-    litersResults = litersResults.toFixed(3);
-    gallonsResults = gallonsResults.toFixed(3);
     let kilogramsResults = input * 2.20462;
     let poundsResults = input * 0.453592;
+    metersResults = metersResults.toFixed(3);
+    feetResults = feetResults.toFixed(3);
+    litersResults = litersResults.toFixed(3);
+    gallonsResults = gallonsResults.toFixed(3);
     kilogramsResults = kilogramsResults.toFixed(3);
     poundsResults = poundsResults.toFixed(3);
-
     lenResults.textContent = input + " meters = " + metersResults + " feet | ";
     lenResults.textContent += input + " feet = " + feetResults + " meters";
     volResults.textContent = input + " liters = " + litersResults + " gallons | ";
     volResults.textContent += input + " gallons = " + gallonsResults + " liters";
     massResults.textContent = input + " kilograms = " + kilogramsResults + " pounds | ";
     massResults.textContent += input + " pounds = " + poundsResults + " kilograms";
-    baseNum.textContent = input;
 }
 
 calculateConversion(input);
